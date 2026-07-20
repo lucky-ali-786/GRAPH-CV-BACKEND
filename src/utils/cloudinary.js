a5d51850ce0e02f942df6f1dd23887a9225e2db5
lucky ali <luckyali7826@gmail.com>
2026-07-20 23:27:11 +0530

    Fix Cloudinary config to use environment variables

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,12 +1,14 @@
+import dotenv from 'dotenv'
 import { v2 as cloudinary } from 'cloudinary'
 import fs from "fs"
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 const uploadFileOnCloudinary = async (localPathFile) => {
     try {
         if (!localPathFile) return;
         cloudinary.config({
             cloud_name: "drqnl2neh",
-            api_key: "process.env.CLOUDINARY_API_KEY",
-            api_secret: "process.env.CLOUDINARY_API_SECRET"
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET
         });
         const response = await cloudinary.uploader.upload(localPathFile, {
             resource_type: "auto"
