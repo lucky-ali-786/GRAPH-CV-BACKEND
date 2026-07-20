e519b8c82c3626caeac9bcf022716e8e2abb979f
lucky ali <luckyali7826@gmail.com>
2026-07-20 23:38:29 +0530

    Add path and URL utilities for Cloudinary integration

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,6 +1,10 @@
 import dotenv from 'dotenv'
 import { v2 as cloudinary } from 'cloudinary'
 import fs from "fs"
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 const uploadFileOnCloudinary = async (localPathFile) => {
     try {
